bf18cfc6b076979f70565fc265f117edd78bf062
bruno.mollo <bruno.mollo@syloper.com>
2025-12-28 01:20:26 -0300

    simplify test for AuthenticationTest

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -5,22 +5,19 @@ use App\Models\User;
 
 test('login screen can be rendered', function () {
     /** @var \Pest\TestCase $this */
-    $response = $this->get('/login');
-
-    $response->assertStatus(200);
+    $this->get('/login')->assertStatus(200);
 });
 
 test('users can authenticate using the login screen', function () {
     /** @var \Pest\TestCase $this */
     $user = User::factory()->create();
 
-    $this->get('/login'); // boot session + token
-
-    $response = $this->post('/login', [
-        '_token' => csrf_token(),
-        'email' => $user->email,
-        'password' => 'password',
-    ]);
+    $response = $this
+        ->from('/login')
+        ->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
 
     $this->assertAuthenticated();
     $response->assertRedirect(route('dashboard', absolute: false));
@@ -30,13 +27,11 @@ test('users can not authenticate with invalid password', function () {
     /** @var \Pest\TestCase $this */
     $user = User::factory()->create();
 
-    $this->get('/login'); // boot session + token
-
-    $this->post('/login', [
-        '_token' => csrf_token(),
-        'email' => $user->email,
-        'password' => 'wrong-password',
-    ]);
+    $this->from('/login')
+        ->post('/login', [
+            'email' => $user->email,
+            'password' => 'wrong-password',
+        ]);
 
     $this->assertGuest();
 });
@@ -45,17 +40,7 @@ test('users can logout', function () {
     /** @var \Pest\TestCase $this */
     $user = User::factory()->create();
 
-    $this->get('/login'); // boot session + token
-
-    $response = $this->post('/login', [
-        '_token' => csrf_token(),
-        'email' => $user->email,
-        'password' => 'password',
-    ]);
-
-    $response = $this->actingAs($user)->post('/logout', [
-        '_token' => csrf_token(),
-    ]);
+    $response = $this->actingAs($user)->post('/logout');
 
     $response->assertRedirect('/');
     $this->assertGuest();

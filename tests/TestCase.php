83f769f6ee5a40a7cb8b00d202eec7485f7bb912
bruno.mollo <bruno.mollo@syloper.com>
2025-12-28 01:03:37 -0300

    disable crsf token for pest

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,14 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        // Disable CSRF middleware for all tests
+        $this->withoutMiddleware([
+            \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+            \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+        ]);
+    }
 }
